7bfdd7cb7870701dad049810c6b6e11ea006f6d7
Fortnitepooppoouuu <fopeyd@gmail.com>
2024-11-09 11:32:08 -0600

    Add files via upload

---
+++ FIrey is has 0 earned/main.js
@@ -0,0 +1,17 @@
+function redirect(){
+    window.location.href = "https://youtube.com/@firey1235?si=VTHx3Ksd0VJ0dnFV";
+}
+
+function badPlayer() {
+  window.location.href = "https://www.youtube.com/channel/UCTWGOaJTDlsPp7s3-qJ_oSg";
+}
+
+function myRank() {
+ window.location.href = "https://fortnitetracker.com/profile/all/Firey%201235/competitive";
+}
+
+function current() {alert ("Someone plz coach me!");}
+
+
+
+function frfr() {alert ("frfr!");}
